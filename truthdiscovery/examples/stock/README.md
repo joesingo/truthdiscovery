90b705809978a063b581ffc5741ff56555928391
Joe Singleton <joesingo@gmail.com>
2019-03-04 00:32:10 +0000

    Add 'stock' example dataset and code

---
+++ truthdiscovery/examples/stock/README.md
@@ -0,0 +1,64 @@
+# Stock dataset example
+
+This directory contains an example of using truth-discovery on a real-world
+dataset. The data is the 'stock' dataset from ["Data Sets for Data Fusion
+Experiments"](http://lunadong.com/fusionDataSets.htm), which contains
+information about 1,000 stock symbols from 55 sources on each week day in July
+2011.
+
+## Data format
+
+The `data` directory inside `data.zip` contains a file for each day in
+*tab-separated values* format. Each line in each file lists the values claimed
+by a source on a given date for several attributes relating to a stock symbol.
+The format in this repo is the same as in the original data, except that the
+date has been added as the first column (in `YYYY-MM-DD` format). The columns
+are:
+
+* Date
+* Source
+* Symbol
+* Change %
+* Last trading price
+* Open price
+* Change $
+* Volume Today's high
+* Today's low Previous close
+* 52wk High
+* 52wk Low
+* Shares Outstanding P/E Market cap
+* Yield
+* Dividend
+* EPS
+
+Note that there are no header rows.
+
+Dates were added to each file so that the files can be concatenated to perform
+truth-discovery on *all* of the data (warning: it took almost *10 minutes* to
+load all the data on my laptop...). In a Unix environment one can simply do
+```bash
+cat data/data-*.tsv > all_data.tsv
+```
+and run the `stock_dataset.py` script on `all_data.tsv`.
+
+## True values
+
+A gold standard dataset of these attributes for 100 NASDAQ stocks is also
+provided in the original data (collected by the original authors from
+[nasdaq.com](https://nasdaq.com)). In this repo the files are in the `truth`
+directory inside the zip, in the same format as `data` but without the `source`
+column. As with the claims data, these files can be concatenated to perform
+accuracy calculations using all the available data.
+
+## Experiments
+
+This directory contains a script `stock_dataset.py` which runs each of the
+supported algorithms on the stock data, and measures the accuracy with respect
+to the NASDAQ truth data.
+
+Values in the source's claims and in the true values are 'cleaned' to account
+for differences in formatting by removing any `$`, `%`, `+` or `-` characters,
+and removing leading and trailing whitespace.
+
+This script also serves as an example of using the `FileDataset` and
+`FileSupervisedData` helper classes.
---
+++ truthdiscovery/examples/stock/stock_dataset.py
@@ -0,0 +1,107 @@
+"""
+Script to load a real-world dataset of stock market data, and perform
+truth-discovery.
+"""
+import csv
+import sys
+import time
+
+from truthdiscovery.algorithm import (
+    AverageLog,
+    Investment,
+    MajorityVoting,
+    PooledInvestment,
+    Sums,
+    TruthFinder
+)
+from truthdiscovery.input import FileDataset, FileSupervisedData
+
+
+class StockBase:
+    """
+    Base class to provide functions required for parsing both data and truth
+    files
+    """
+    def get_tsv_reader(self, fileobj):
+        return csv.reader(fileobj, delimiter="\t")
+
+    def get_variable_name(self, symbol, date, attr_index):
+        return "{}_{}_attr_{}".format(symbol, date, attr_index)
+
+    def clean_value(self, value):
+        bad_chars = ("$", "%", "+", "-")
+        for char in bad_chars:
+            value = value.replace(char, "")
+        return value.strip()
+
+    def get_var_vals(self, row, date):
+        """
+        Yield tuples of the form (var, val) for each value in the given row.
+        Note that ``row`` should not include the source or date.
+        """
+        symbol, *attributes = row
+        for i, attr in enumerate(attributes):
+            var = self.get_variable_name(symbol, date, i)
+            val = self.clean_value(attr)
+            if val:
+                yield (var, val)
+
+
+class StockDataset(FileDataset, StockBase):
+    def get_tuples(self, fileobj):
+        for row in self.get_tsv_reader(fileobj):
+            date, source, *rest = row
+            for var, val in self.get_var_vals(rest, date):
+                yield (source, var, val)
+
+
+class SupervisedStockData(FileSupervisedData, StockBase):
+    def get_pairs(self, fileobj):
+        for row in self.get_tsv_reader(fileobj):
+            date, *rest = row
+            yield from self.get_var_vals(rest, date)
+
+
+def usage(stream=sys.stdout):
+    print("usage: {} DATA_TSV TRUTH_TSV".format(sys.argv[0]), file=stream)
+
+
+def main():
+    # Show usage
+    if len(sys.argv) > 1 and sys.argv[1] in ("-h", "--help"):
+        usage()
+        sys.exit(0)
+
+    try:
+        data_path, truth_path = sys.argv[1:]
+    except ValueError:
+        usage(sys.stderr)
+        sys.exit(1)
+
+    print("loading data...")
+    start = time.time()
+    dataset = StockDataset(data_path)
+    end = time.time()
+    print("  loaded in {:.3f} seconds".format(end - start))
+
+    print("loading true values...")
+    start = time.time()
+    sup = SupervisedStockData(dataset, truth_path)
+    end = time.time()
+    print("  loaded in {:.3f} seconds".format(end - start))
+    print("")
+
+    algorithms = [
+        MajorityVoting(), Sums(), AverageLog(), Investment(),
+        PooledInvestment(), TruthFinder()
+    ]
+
+    for alg in algorithms:
+        print("running {}...".format(alg.__class__.__name__))
+        res = alg.run(sup.data)
+        acc = sup.get_accuracy(res)
+        print("  {:.3f} seconds, {:.3f} accuracy".format(res.time_taken, acc))
+
+
+if __name__ == "__main__":
+    main()
