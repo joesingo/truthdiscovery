bd44e4ac4a004a3fa3721636b1b2eab25e50f9e6
Joe Singleton <joesingo@gmail.com>
2019-03-28 13:21:28 +0000

    Modify animation example

--- truthdiscovery/examples/animation.py
+++ truthdiscovery/examples/animation.py
@@ -1,8 +1,10 @@
 import sys
 
+import numpy.ma as ma
+
 from truthdiscovery.algorithm import *
-from truthdiscovery.input import Dataset
-from truthdiscovery.graphs import Animator, GraphRenderer
+from truthdiscovery.input import *
+from truthdiscovery.graphs import *
 from truthdiscovery.utils import ConvergenceIterator, DistanceMeasures
 
 
@@ -13,24 +15,33 @@ if __name__ == "__main__":
         print("usage: {} DEST".format(sys.argv[0]), file=sys.stderr)
         sys.exit(1)
 
-    tuples = [
-        ("source 1", "x", 4),
-        ("source 1", "y", 7),
-        ("source 2", "y", 7),
-        ("source 2", "z", 5),
-        ("source 3", "x", 3),
-        ("source 3", "z", 5),
-        ("source 4", "x", 3),
-        ("source 4", "y", 6),
-        ("source 4", "z", 8)
-    ]
-    mydata = Dataset(tuples)
+#     tuples = [
+#         ("source 1", "x", 4),
+#         ("source 1", "y", 7),
+#         ("source 2", "y", 7),
+#         ("source 2", "z", 5),
+#         ("source 3", "x", 3),
+#         ("source 3", "z", 5),
+#         ("source 4", "x", 3),
+#         ("source 4", "y", 6),
+#         ("source 4", "z", 8)
+#     ]
+#     mydata = Dataset(tuples)
 
-    # it = ConvergenceIterator(DistanceMeasures.L2, 0.001)
-    # algorithm = Investment(iterator=it)
-    algorithm = Investment()
+    mydata = MatrixDataset(ma.masked_values([
+        [1, 9, 3, 4],
+        [2, 2, 9, 9],
+        [9, 9, 7, 9],
+        [1, 2, 5, 9]
+    ], 9))
 
-    rend = GraphRenderer()
-    animator = Animator(renderer=rend, frame_duration=0.2)
+    it = ConvergenceIterator(DistanceMeasures.L2, 0.001)
+    algorithm = Investment(iterator=it)
+
+    cs = ResultsGradientColourScheme(algorithm.run(mydata))
+    rend = MatrixDatasetGraphRenderer(zero_indexed=False, colours=cs)
     with open(out_path, "wb") as outfile:
-        animator.animate(outfile, algorithm, mydata)
+        rend.draw(mydata, outfile)
+    # animator = Animator(renderer=rend, frame_duration=0.2)
+    # with open(out_path, "wb") as outfile:
+    #     animator.animate(outfile, algorithm, mydata)
