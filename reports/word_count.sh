2f9809e2e06940e38a2efedb220985713f2b7c96
Joe Singleton <joesingo@gmail.com>
2019-04-26 13:17:10 +0100

    Remove reports directory
    Reports have been moved to a separate repo

--- reports/word_count.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-base_dir="/home/joe/uni/project/repo/reports/"
-log_file="${base_dir}/wc.csv"
-count=$(find "${base_dir}/" -type f -name "*.tex" | xargs detex | wc -w)
-timestamp=$(date +%s)
-echo "$timestamp,$count" | tee -a "$log_file"
