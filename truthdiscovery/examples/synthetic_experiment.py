9f50b90a402f6daf6a7191de7da6c9c9cd620848
Joe Singleton <joesingo@gmail.com>
2019-05-07 11:38:19 +0100

    Add more example experiments to synthetic_experiment.py

--- truthdiscovery/examples/synthetic_experiment.py
+++ truthdiscovery/examples/synthetic_experiment.py
@@ -1,8 +1,11 @@
 """
-Small script to run various algorithms on synthetic datasets, calculate average
-accuracy, and produce graphs with results.
+Script to run various experiments measuring the accuracy of algorithms on
+synthetic datasets, and graphing results
 """
 from collections import OrderedDict
+import itertools
+import json
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,84 +21,233 @@ from truthdiscovery.algorithm import (
 )
 
 
-NUM_SOURCES = 100
-NUM_VARIABLES = 100
-CLAIM_PROBABILITY = 0.1
-DOMAIN_SIZE = 4
-REPETITIONS = 10
+ALGORITHMS = OrderedDict({
+    "Voting": MajorityVoting(),
+    "Sums": Sums(),
+    "Average.Log": AverageLog(),
+    "Investment": Investment(),
+    "Pooled Investment": PooledInvestment(),
+    "TruthFinder": TruthFinder()
+})
+
+
+class Experiment:
+    # labels for values of independent variable
+    labels = None
+    # dict mapping algorithm labels to objects
+    algorithms = None
+    # number of trials to perform for each value
+    reps = 10
+    # parameters to pass to synthetic data generation. Value for independent
+    # variable should be an iterable of values of callables
+    synth_params = None
+
+    def __init__(self):
+        for key, val in self.synth_params.items():
+            # If not an iterable, repeat the value endlessly
+            try:
+                self.synth_params[key] = iter(val)
+            except TypeError:
+                self.synth_params[key] = itertools.repeat(val)
+
+    def run(self):
+        out = {}
+        for var_label in self.labels:
+            # results dict: {alg_label: [acc, ...], ...}
+            results = {}
+            params = dict(self.get_synth_params())
+            for _ in range(self.reps):
+                this_params = {}
+                for key, val in params.items():
+                    try:
+                        this_params[key] = val()
+                    except TypeError:
+                        this_params[key] = val
+
+                synth = SyntheticData(**this_params)
+                for alg_label, alg in self.algorithms.items():
+                    acc = synth.get_accuracy(alg.run(synth.data))
+                    this_res = results.setdefault(alg_label, [])
+                    this_res.append(acc)
+            out[var_label] = results
+        return out
+
+    def get_synth_params(self):
+        def inner():
+            for key, seq in self.synth_params.items():
+                val = next(seq)
+                yield key, val
+        return dict(inner())
+
+    @classmethod
+    def run_multiple(cls, *args):
+        out = {}
+        for exp in args:
+            name = exp.__class__.__name__
+            print("running {}...".format(name), file=sys.stderr)
+            out[name] = exp.run()
+        return out
+
+    @classmethod
+    def graph(cls, res):
+        raise NotImplementedError
+
+
+class TrustDistExperiment(Experiment):
+    algorithms = ALGORITHMS
+    labels = None  # set in constructor
+    num_sources = 100
+    synth_params = {
+        "num_variables": 100,
+        "claim_probability": 0.1,
+        "domain_size": 5,
+        "trust": None  # set in constructor
+    }
+
+    def __init__(self):
+        trust_dists = OrderedDict({
+            "Mostly Bad": np.array(list(
+                0.75 if i < self.num_sources / 3 else 0.25
+                for i in range(self.num_sources)
+            )),
+            "Uniform": lambda: np.random.uniform(size=(self.num_sources,)),
+            r"Normal ($\mu=0.5$, $\sigma=0.15$)": lambda: np.clip(
+                np.random.normal(0.5, 0.15, size=(self.num_sources,)),
+                0, 1
+            ),
+        })
+        self.labels = trust_dists.keys()
+        self.synth_params["trust"] = trust_dists.values()
+        super().__init__()
+
+    @classmethod
+    def graph(cls, res):
+        # Plot trust experiment as bar chart
+        fig, ax = plt.subplots()
+        num_trust_dists = len(res)
+        bar_width = 0.8 / num_trust_dists
+        alg_labels = list(cls.algorithms.keys())
+        index = np.arange(len(alg_labels))
+        for i, (dist, results) in enumerate(res.items()):
+            # Get mean accuracy scores for all algorithms for this trust
+            # distribution
+            acc_scores = list(np.mean(results[alg]) for alg in alg_labels)
+            ax.bar(
+                index + i * bar_width,
+                acc_scores,
+                bar_width,
+                align="edge",
+                label=dist
+            )
+
+        ax.set_title(
+            "Source trust distribution experiment ({} sources, {} variables, "
+            "{} domain values)"
+            .format(cls.num_sources, cls.synth_params["num_variables"],
+                    cls.synth_params["domain_size"])
+        )
+        ax.set_xlabel("Algorithm")
+        ax.set_ylabel("Mean accuracy over {} trials".format(cls.reps))
+        ax.set_xticks(index + bar_width * num_trust_dists / 2)
+        ax.set_xticklabels(alg_labels)
+        ax.legend()
+        fig.tight_layout()
+
+
+class ClaimDensityExperiment(Experiment):
+    labels = None  # set in constructor
+    algorithms = ALGORITHMS
+    synth_params = {
+        "trust": lambda: np.random.uniform(size=(100,)),
+        "num_variables": 100,
+        "claim_probability": None,  # set in constructor
+        "domain_size": 10
+    }
+
+    def __init__(self):
+        claim_probs = list(np.clip(np.arange(0.1, 1.025, 0.05), 0, 1))
+        self.synth_params["claim_probability"] = claim_probs
+        self.labels = claim_probs
+        super().__init__()
+
+    @classmethod
+    def graph(cls, res):
+        _fig, ax = plt.subplots()
+        # JSON does not support non-string keys, so convert to float
+        xs = list(map(float, res.keys()))
+        for alg in cls.algorithms:
+            ys = [np.mean(res[str(x)][alg]) for x in xs]
+            ax.plot(xs, ys, "x-", label=alg, linewidth=3)
+
+        ax.set_title(
+            "Claim probability experiment ({} sources, {} variables, {} "
+            "domain values)"
+            .format(100, cls.synth_params["num_variables"],
+                    cls.synth_params["domain_size"])
+        )
+        ax.set_xlabel("Claim probability")
+        ax.set_ylabel("Mean accuracy over {} trials".format(cls.reps))
+        ax.legend()
+
+
+class DomainSizeExperiment(Experiment):
+    labels = None
+    algorithms = ALGORITHMS
+    synth_params = {
+        "trust": lambda: np.random.uniform(size=(100,)),
+        "num_variables": 100,
+        "claim_probability": 0.1,
+        "domain_size": None
+    }
+
+    def __init__(self):
+        domain_sizes = list(range(2, 21))
+        self.labels = domain_sizes
+        self.synth_params["domain_size"] = domain_sizes
+        super().__init__()
+
+    @classmethod
+    def graph(cls, res):
+        _fig, ax = plt.subplots()
+        xs = list(map(int, res.keys()))
+        for alg in cls.algorithms:
+            ys = [np.mean(res[str(x)][alg]) for x in xs]
+            ax.plot(xs, ys, "x-", label=alg, linewidth=3)
+
+        ax.set_title(
+            "Domain size experiment ({} sources, {} variables, {} "
+            "claim probability)"
+            .format(100, cls.synth_params["num_variables"],
+                    cls.synth_params["claim_probability"])
+        )
+        ax.set_xlabel("Variable domain size")
+        ax.set_ylabel("Mean accuracy over {} trials".format(cls.reps))
+        ax.legend()
 
 
 def main():
-    trust_distributions = OrderedDict({
-        # "fixed-0.25": lambda size: np.full(size, 0.25),
-        # "fixed-0.5": lambda size: np.full(size, 0.5),
-        # "fixed-0.75": lambda size: np.full(size, 0.75),
-        # "fixed-range": lambda size: np.linspace(0, 1, num=size[0]),
-        "mostly bad": lambda size: np.array(list(
-            0.75 if i < size[0] / 3 else 0.25 for i in range(size[0])
-        )),
-        "uniform": np.random.uniform,
-        r"normal ($\mu=0.5$, $\sigma=0.15$)": lambda size: np.random.normal(
-            0.5, 0.15, size=size
-        ),
-    })
-    algorithms = OrderedDict({
-        "voting": MajorityVoting(),
-        "sums": Sums(),
-        "average.log": AverageLog(),
-        "investment": Investment(),
-        "Pooled Investment": PooledInvestment(),
-        "TruthFinder": TruthFinder()
-    })
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print("usage: {}".format(sys.argv[0]))
+        print("       {} graph FILE\n".format(sys.argv[0]))
+        print("in the first form, run the experiment and print results as "
+              "JSON to stdout")
+        print("in the second, graph the results from the given file")
+        return
 
-    results = {}
-    for dist_label, dist in trust_distributions.items():
-        trust = np.clip(dist(size=(NUM_SOURCES,)), 0, 1)
-        for _ in range(REPETITIONS):
-            synth = SyntheticData(
-                trust, NUM_VARIABLES, CLAIM_PROBABILITY, DOMAIN_SIZE
-            )
+    if len(sys.argv) >= 3 and sys.argv[1] == "graph":
+        with open(sys.argv[2]) as json_file:
+            res = json.load(json_file)
+        for cls_name, results in res.items():
+            globals()[cls_name].graph(results)
+        plt.show()
+        return
 
-            for alg_label, alg in algorithms.items():
-                acc = synth.get_accuracy(alg.run(synth.data))
-                key = (alg_label, dist_label)
-                if key not in results:
-                    results[key] = []
-                results[key].append(acc)
-
-    for alg_label in algorithms:
-        print("{}:".format(alg_label))
-        for dist_label in trust_distributions:
-            key = (alg_label, dist_label)
-            mean = np.mean(results[key])
-            print("  {}: {}".format(dist_label, mean))
-
-    # Plot as barchart
-    fig, ax = plt.subplots()
-    bar_width = 0.8 / len(trust_distributions)
-    index = np.arange(len(algorithms))
-    for i, dist in enumerate(trust_distributions):
-        # Get mean accuracy scores for all algorithms for this trust
-        # distribution
-        acc_scores = list(np.mean(results[(alg, dist)]) for alg in algorithms)
-        ax.bar(
-            index + i * bar_width,
-            acc_scores,
-            bar_width,
-            align="edge",
-            label=dist.capitalize()
-        )
-
-    ax.set_title("{} sources, {} variables, {} domain values".format(
-        NUM_SOURCES, NUM_VARIABLES, DOMAIN_SIZE
-    ))
-    ax.set_xlabel("Algorithm")
-    ax.set_ylabel("Mean accuracy over {} trials".format(REPETITIONS))
-    ax.set_xticks(index + bar_width * len(trust_distributions) / 2)
-    ax.set_xticklabels(map(str.capitalize, algorithms.keys()))
-    ax.legend()
-    fig.tight_layout()
-    plt.show()
+    res = Experiment.run_multiple(
+        # ClaimDensityExperiment(),
+        # TrustDistExperiment(),
+        DomainSizeExperiment()
+    )
+    print(json.dumps(res, indent=2))
 
 
 if __name__ == "__main__":
